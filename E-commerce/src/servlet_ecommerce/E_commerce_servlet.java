c4319628612f4f90a9d3e3f63a0c62cc24a0036c
Álvaro García Ochoa <alvarogarciaochoamf@gmail.com>
2019-11-09 22:00:13 +0100

    Funcionalidad de añadir usuario desde la web a la base de datos implementada

--- E-commerce/src/servlet_ecommerce/E_commerce_servlet.java
+++ E-commerce/src/servlet_ecommerce/E_commerce_servlet.java
@@ -87,6 +87,7 @@ public class E_commerce_servlet extends HttpServlet {
 				session.setAttribute("user", email);
 				session.setAttribute("password_user", password );
 			}
+			//falta sacar los valores del boton status
 			Request_Manager myManager = new Request_Manager();
 			myManager.crearUsuario(request.getParameter("Email"), request.getParameter("Password"), 1, request.getParameter("CP"), request.getParameter("Direccion"), request.getParameter("Apellido1"), request.getParameter("Apellido2"), request.getParameter("Nombre"));
 			//TODO 
