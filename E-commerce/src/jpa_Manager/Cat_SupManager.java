df43e7fe31c40f54cfaed2a6dddb86fabaf5d266
Álvaro García Ochoa <alvarogarciaochoamf@gmail.com>
2019-11-09 10:44:40 +0100

    Creacion de funciones para insertar a base de datos por medio de JPA
    y creacion de esqueleto de funciones para borrar, modificar y consultar

---
+++ E-commerce/src/jpa_Manager/Cat_SupManager.java
@@ -0,0 +1,114 @@
+package jpa_Manager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import servlet_ecommerce.CategoríasSuperiore;
+public class Cat_SupManager {
+	private EntityManagerFactory emf;
+	
+	public Cat_SupManager() {
+
+	}
+
+	public Cat_SupManager(EntityManagerFactory emf) {
+		this.emf = emf;
+	}
+
+	public void setEntityManagerFactory(EntityManagerFactory emf) {
+		this.emf = emf;
+	}
+
+	private EntityManager getEntityManager() {
+		if (emf == null) {
+			throw new RuntimeException(
+					"The EntityManagerFactory is null.  This must be passed in to the constructor or set using the setEntityManagerFactory() method.");
+		}
+		return emf.createEntityManager();
+	}
+
+	public String createCategoríasSuperiores(CategoríasSuperiore categoríaSuperior) throws Exception {
+		EntityManager em = getEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.persist(categoríaSuperior);
+			em.getTransaction().commit();
+		} catch (Exception ex) {
+			try {
+				if (em.getTransaction().isActive()) {
+					em.getTransaction().rollback();
+				}
+			} catch (Exception e) {
+				ex.printStackTrace();
+				throw e;
+			}
+			throw ex;
+		} finally {
+			em.close();
+		}
+		return "";
+	}
+
+	public String deletecategoríaSuperior(CategoríasSuperiore categoríaSuperior) throws Exception {
+		EntityManager em = getEntityManager();
+		try {
+			em.getTransaction().begin();
+			categoríaSuperior = em.merge(categoríaSuperior);
+			em.remove(categoríaSuperior);
+			em.getTransaction().commit();
+		} catch (Exception ex) {
+			try {
+				if (em.getTransaction().isActive()) {
+					em.getTransaction().rollback();
+				}
+			} catch (Exception e) {
+				ex.printStackTrace();
+				throw e;
+			}
+			throw ex;
+		} finally {
+			em.close();
+		}
+		return "";
+	}
+
+	public String updatecategoríaSuperior(CategoríasSuperiore categoríaSuperior) throws Exception {
+		EntityManager em = getEntityManager();
+		try {
+			em.getTransaction().begin();
+			categoríaSuperior = em.merge(categoríaSuperior);
+			em.getTransaction().commit();
+		} catch (Exception ex) {
+			try {
+				if (em.getTransaction().isActive()) {
+					em.getTransaction().rollback();
+				}
+			} catch (Exception e) {
+				ex.printStackTrace();
+				throw e;
+			}
+			throw ex;
+		} finally {
+			em.close();
+		}
+		return "";
+	}
+
+	public CategoríasSuperiore findcategoríaSuperiorById(String id) {
+		CategoríasSuperiore categoríaSuperior = null;
+		EntityManager em = getEntityManager();
+		try {
+			categoríaSuperior = (CategoríasSuperiore) em.find(CategoríasSuperiore.class, id);
+		} finally {
+			em.close();
+		}
+		return categoríaSuperior;
+	}
+
+	public CategoríasSuperiore getNewcategoríaSuperior() {
+
+		CategoríasSuperiore categoríaSuperior = new CategoríasSuperiore();
+
+		return categoríaSuperior;
+	}
+}
--- E-commerce/src/request_Manager/Request_Manager.java
+++ E-commerce/src/request_Manager/Request_Manager.java
@@ -25,6 +25,8 @@ public class Request_Manager {
 		
 	}
 	
+/********************************INSERCION EN BASE DE DATOS POR JPA**********************************************************************************************************/
+	
 	public void crear_Producto(String idProducto,String vendedor, int precio,int stock, String cat_Inf,String descripcion, String long_descripcion,byte[] imagen) {
 		Producto newProduct=new Producto();
 		newProduct.setDescription(descripcion);
@@ -59,10 +61,144 @@ public class Request_Manager {
 		try {
 			manager.createProducto(newProduct);
 		} catch (Exception e) {
-			System.out.println("Descripciï¿½n: " + e.getMessage());
+			System.out.println("Descripción: " + e.getMessage());
 		}
 
 		
 	}
 	
+	public void crearUsuario(String email, String contraseña, int status, String Cpostal, String direccion, String apellido, String apellido2, String nombre) {
+		Usuario newUser=new Usuario();
+		newUser.setEmail(email);
+		newUser.setApellido(apellido);
+		newUser.setApellido2(apellido2);
+		newUser.setContrasena(contraseña);
+		newUser.setCpostal(Cpostal);
+		newUser.setEstatus(status);
+		newUser.setNombre(nombre);
+		newUser.setDirección(direccion);
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("EjemploJPA");
+		UsuariosManager manager=new UsuariosManager();
+		manager.setEntityManagerFactory(factory);
+		try {
+			manager.createUsuario(newUser);
+		}catch(Exception e) {
+			System.out.println("Descripción: " + e.getMessage());
+		}
+	}
+	
+	public void crearCatInf(String nombre_Cat_inf,String nombre_Cat_Sup) {
+		CategoríasInferiore newCat_Inf=new CategoríasInferiore();
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("EjemploJPA");
+		Cat_SupManager sup=new Cat_SupManager();
+		sup.setEntityManagerFactory(factory);
+		try {
+			CategoríasSuperiore cat=sup.findcategoríaSuperiorById(nombre_Cat_Sup);
+			newCat_Inf.setCategoríasSuperiore(cat);
+		
+		}catch(Exception e) {
+			System.out.println("Descripción: "+ e.getMessage());
+		}
+		
+		Cat_InfManager manager=new Cat_InfManager();
+		manager.setEntityManagerFactory(factory);
+		try {
+			manager.createCategoríasInferiores(newCat_Inf);
+		}catch(Exception e) {
+			System.out.println("Descripción: " + e.getMessage());
+		}
+	}
+		
+	public void crearCatSup(String nombre_Cat_Sup) {
+		CategoríasSuperiore newCat_Sup=new CategoríasSuperiore();
+		newCat_Sup.setNombre_Cat_Sup(nombre_Cat_Sup);
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("EjemploJPA");
+		Cat_SupManager manager=new Cat_SupManager();
+		manager.setEntityManagerFactory(factory);
+		try {
+			manager.createCategoríasSuperiores(newCat_Sup);
+			
+		
+		}catch(Exception e) {
+			System.out.println("Descripción: "+ e.getMessage());
+		}
+	
+		
+	}
+	
+	public void crearPedido(int tipo, String usuario, int cantidad, String producto, int numeropedido) {
+		Pedido newPedido=new Pedido();
+		newPedido.setTipo(tipo);
+		newPedido.setCantidad(cantidad);
+		newPedido.setNºPedido(numeropedido);
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("EjemploJPA");
+		UsuariosManager usu=new UsuariosManager();
+		usu.setEntityManagerFactory(factory);
+		try {
+			Usuario user=usu.findusuarioById(usuario);
+			newPedido.setUsuario(user);
+		}catch(Exception e) {
+			System.out.println("Descripción: "+ e.getMessage());
+
+		}
+		ProductoManager prod=new ProductoManager();
+		prod.setEntityManagerFactory(factory);
+		try {
+			Producto product=prod.findproductoById(producto);
+			newPedido.setProducto(product);
+		}catch(Exception e) {
+			System.out.println("Descripción: "+ e.getMessage());
+
+		}
+		PedidosManager pedido=new PedidosManager();
+		pedido.setEntityManagerFactory(factory);
+		try {
+			pedido.createPedido(newPedido);
+		}catch(Exception e) {
+			System.out.println("Descripción: "+ e.getMessage());
+
+		}
+	}
+
+/***************************************ELIMINACION EN BASE DE DATOS POR JPA****************************************************/
+	public void eliminarPedido(int pedido) {
+		
+	}
+	
+	public void eliminarUsuario() {
+		
+	}
+	
+	public void eliminarProducto() {
+		
+	}
+	
+	public void eliminarCat_Inf() {
+		
+	}
+	
+	public void eliminarCat_Sup() {
+		
+	}
+	
+	
+/***************************************MODIFICACION EN BASE DE DATOS POR JPA*****************************************************/
+	public Usuario modificarUsuario() {
+		Usuario usermod=new Usuario();
+		return usermod;
+	}
+	
+	public Pedido modificarPedido() {
+		Pedido pedidomod=new Pedido();
+		return pedidomod;
+	}
+	
+	public Producto modificarProducto() {
+		Producto productomod=new Producto();
+		return productomod;
+	}
+	
+/**************************************CONSULTA A BASE DE DATOS POR JPA***************************************************************/
+	
+
 }
